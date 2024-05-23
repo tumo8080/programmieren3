2d901aaf8689394dc910de52ffbbbf75c7279955
niemand8080 <tumo.github@niemand8080.de>
2024-05-23 18:25:20 +0200

    uebung 2 done

--- SpielDesLebensAbschlusprojekt(Fertig)/index.js
+++ SpielDesLebensAbschlusprojekt(Fertig)/index.js
@@ -8,3 +8,8 @@ app.get("/", (req, res) => {
 app.listen(3000, function(){
    console.log("Example is running on port 3000");
 });
+
+app.get("/user/:name", function(req, res){
+    const name = req.params.name;
+    res.send("<h1>Hello " + name +"</h1>");
+ });
